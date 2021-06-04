803b94736dea77e0a81ff8ca7a39d8094da671b7
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-06-04 22:04:28 +0900

    Modified index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,40 +6,60 @@ import {
   Area,
   Tooltip,
   CartesianGrid,
-} from "recharts";
-import { format, parseISO, subDays } from "date-fns";
+} from 'recharts';
+import { format, parseISO, subDays } from 'date-fns';
 
 const data = [];
 for (let num = 30; num >= 0; num--) {
+  // console.log(subDays(new Date(), num).toISOString().substr(0, 10));
+  console.log(data);
   data.push({
     date: subDays(new Date(), num).toISOString().substr(0, 10),
-    value: 1 + Math.random(),
+    value: 0.1 + Math.random() - 0.3
   });
 }
+
+const gradientOffset = () => {
+  const dataMax = Math.max(...data.map((i) => i.value));
+  const dataMin = Math.min(...data.map((i) => i.value));
+
+  if (dataMax <= 0) {
+    return 0;
+  }
+  if (dataMin >= 0) {
+    return 1;
+  }
+
+  return dataMax / (dataMax - dataMin);
+};
+
+const off = gradientOffset();
 
 export default function Home() {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <AreaChart data={data}>
         <defs>
-          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
-            <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+          <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+            <stop offset={off} stopColor="#ADD49B" stopOpacity={1} />
+            <stop offset={off} stopColor="#E28DB2" stopOpacity={1} />
           </linearGradient>
         </defs>
 
-        <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
+        <Area type="monotone" dataKey="value" stroke="rgba(0,0,0,0)" fill="url(#splitColor)" />
 
+        <CartesianGrid strokeDasharray="4 4" />
         <XAxis
           dataKey="date"
           axisLine={false}
           tickLine={false}
           tickFormatter={(str) => {
             const date = parseISO(str);
-            if (date.getDate() % 7 === 0) {
-              return format(date, "MMM, d");
+            console.log(date);
+            if (date.getDate() % 3 === 0) {
+              return format(date, 'MMM, d');
             }
-            return "";
+            return '';
           }}
         />
 
@@ -63,7 +83,7 @@ function CustomTooltip({ active, payload, label }) {
   if (active) {
     return (
       <div className="tooltip">
-        <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4>
+        <h4>{format(parseISO(label), 'eeee, d MMM, yyyy')}</h4>
         <p>${payload[0].value.toFixed(2)} CAD</p>
       </div>
     );
